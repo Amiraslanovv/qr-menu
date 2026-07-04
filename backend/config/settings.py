0dcda9d615dfcaef5a2433a38f7c6c283d54cb86
Amiraslanovv <aamiraslanov.52192@student.naa.edu.az>
2026-07-04 13:30:34 +0400

    csrf ve login duzeldildi

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -80,10 +80,12 @@ CSRF_TRUSTED_ORIGINS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
     ],
 }
 
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("api/", include("menu.urls")),
-    path("api/auth/login/", obtain_auth_token, name="api_token_auth"),
     # Frontend səhifələri
     path("menu/<slug:slug>/", TemplateView.as_view(template_name="menu.html"), name="menu_page"),
     path("admin-panel/", TemplateView.as_view(template_name="admin_panel.html"), name="admin_panel"),
